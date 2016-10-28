26d04dcc4742648139430424c20ceaa2e5042687
IgnacioCasales <ignaciocasales_nqcho@hotmail.com>
2016-10-28 11:49:57 -0300

    Refactor

--- src/index.php
+++ src/index.php
@@ -4,5 +4,14 @@
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
 
+	require "Config/Autoload";
+	require "Config/Config";
 	
+	Config/autoload::iniciar();
+	
+	include URL_THEME . 'header.php';
+	
+	Config/Router::direccionar(new Config/Request());
+	
+	include URL_THEME . 'footer.php';
 ?>
