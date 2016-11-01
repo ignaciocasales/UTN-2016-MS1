4d7ff4d537f0154d9f41a023618be6ffbe78bd46
IgnacioCasales <ignaciocasales_nacho@hotmail.com>
2016-11-01 12:09:34 -0300

    v

---
+++ src/Vistas/login.php
@@ -0,0 +1,23 @@
+<div class="container login box">
+    <div class="bg"></div>
+    <div class="row">
+        <div class="hidden-xs hidden-sm col-md-8 bienvenida">
+            <h2>Bienvenido</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquid aperiam delectus dolorum exercitationem explicabo facere facilis fuga impedit, nesciunt numquam quaerat quibusdam quidem ratione recusandae sed suscipit temporibus voluptas.</p>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-4">
+            <h3>Inicie Sesión</h3>
+            <form>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
