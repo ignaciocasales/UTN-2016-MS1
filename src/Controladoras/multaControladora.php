a0aee73d65bd1fe73811373e8556f81a2d8e9ece
IgnacioCasales <ignaciocasales_nacho@hotmail.com>
2016-11-02 11:51:09 -0300

    2/11

---
+++ src/Controladoras/multaControladora.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ignacio
+ * Date: 2/11/2016
+ * Time: 11:18 AM
+ */
+
+namespace Controladoras;
+
+
+class multaControladora
+{
+
+}
---
+++ src/Dao/RolBdDao.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dao;
+
+
+class RolBdDao
+{
+
+}
