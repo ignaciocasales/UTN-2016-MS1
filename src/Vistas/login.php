ea3783df22e44a085a033e33b06fe170193ec496
IgnacioCasales <ignaciocasales_nacho@hotmail.com>
2016-11-07 17:34:39 -0300

    hotfix mensaje

--- src/Vistas/login.php
+++ src/Vistas/login.php
@@ -2,14 +2,7 @@
 <div class="container lower-box box-primary">
     <div class="bg"></div>
     <div class="row">
-        <div class="hidden-xs hidden-sm col-md-8 box-right-content">
-            <h2>Bienvenido</h2>
-            <p>La Municipalidad está planeando la informatización y automatización del pago de peajes y la registración
-                de multas por pasaje de semáforo en rojo o exceso de velocidad en las arterias urbanas. Para
-                cumplimentar dicha tarea se desarrolló un sistema prototípico que realiza la simulación del proceso
-                hasta tanto no se compre la infraestructura adecuada ni se realice la legislación respectiva que
-                garantice y ponga a punto el sistema.</p>
-        </div>
+        <?php include ("mensaje.php")?>
         <div class="col-xs-12 col-sm-12 col-md-4">
             <h3>Iniciar Sesión</h3>
             <form method="post" action="/login/verificar/">
---
+++ src/Vistas/mensaje.php
@@ -0,0 +1,8 @@
+<div class="hidden-xs hidden-sm col-md-8 box-right-content">
+    <h2>Bienvenido</h2>
+    <p>La Municipalidad está planeando la informatización y automatización del pago de peajes y la registración
+        de multas por pasaje de semáforo en rojo o exceso de velocidad en las arterias urbanas. Para
+        cumplimentar dicha tarea se desarrolló un sistema prototípico que realiza la simulación del proceso
+        hasta tanto no se compre la infraestructura adecuada ni se realice la legislación respectiva que
+        garantice y ponga a punto el sistema.</p>
+</div>
