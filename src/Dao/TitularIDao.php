7e1c0681e464bcb66c3b92d06196041203828b58
Damian Tacconi <dami_tano_95@hotmail.com>
2016-11-12 21:39:22 -0300

    TitularIDao.php

--- src/Dao/TitularIDao.php
+++ src/Dao/TitularIDao.php
@@ -5,5 +5,15 @@ namespace Dao;
 
 interface TitularIDao
 {
+    public function agregar($titular);
 
+    public function eliminar($dni);
+
+    public function actualizar($titular);
+
+    public function traeTodo();
+
+    public function traerPorDni($dni);
+
+    public function mapear($dataSet);
 }
