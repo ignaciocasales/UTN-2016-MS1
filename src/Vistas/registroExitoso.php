4f747725d0d9eae2149a4c36060f0dbdd8ed1664
IgnacioCasales <ignaciocasales_nacho@hotmail.com>
2016-11-16 02:14:58 -0300

    refactorizacion vistas 3

--- src/Vistas/registroExitoso.php
+++ src/Vistas/registroExitoso.php
@@ -1,12 +1,16 @@
 <?php include('navbar.php'); ?>
-<header>
-    <h2 style="text-align: center;">Se registro el <?= $nombre ?> con Exito!</h2>
-</header>
 <div class="container">
     <div class="bg"></div>
     <div class="row">
-        <div class="col-md-3 col-md-offset-4">
-            <img src="/img/exito.png">
+        <div class="col-xs-6 col-md-offset-3 lower-box box-primary bg-success">
+            <div class="row">
+                <div class="col-xs-2">
+                    <img src="/img/exito.png" alt="exito" class="img-responsive">
+                </div>
+                <div class="col-xs-10">
+                    <h3>Se registro el <?= $nombre ?> con éxito !</h3>
+                </div>
+            </div>
         </div>
     </div>
 </div>
