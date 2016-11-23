34aac997b19a7f558af52a0a686bef56bf47e47b
navarroddi <davidddi.dn@gmail.com>
2016-11-23 20:13:43 -0300

    daos y clases para google maps

--- src/Vistas/mapsSensor.php
+++ src/Vistas/mapsSensor.php
@@ -1,33 +1,33 @@
 <?php include('navbar.php'); ?>
 <div class="container lower-box box-primary">
-  <div clas ="container" >
+    <div clas ="container" >
 
 
-      <input type="button" onclick="init()" value="Mostrar mapa">
-      <div id="map" style="width: 800px; height: 800px;"></div>
+        <input type="button" onclick="init()" value="Mostrar mapa">
+        <div id="map" style="width: 1137px; height: 380px;"></div>
 
-      <script src="http://maps.google.com/maps/api/js?sensor=false"> </script>
+        <script src="http://maps.google.com/maps/api/js?sensor=false"> </script>
 
-      <script type="text/javascript">
-          var map;
-          var marker;
+        <script type="text/javascript">
+            var map;
+            var marker;
 
-          function init() {
-              var mapOptions = {
-                  center: new google.maps.LatLng(-38.0526,-57.5614),
-                  zoom: 17,
-                  mapTypeId: google.maps.MapTypeId.ROADMAP
-              }
-              map = new google.maps.Map(document.getElementById("map"),mapOptions);
+            function init() {
+                var mapOptions = {
+                    center: new google.maps.LatLng(-38.0526,-57.5614),
+                    zoom: 17,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                }
+                map = new google.maps.Map(document.getElementById("map"),mapOptions);
 
-              var place = new google.maps.LatLng(-38.0526,-57.5614);
-              marker = new google.maps.Marker({
-                  position: place,
-                  title: "sdasdasd",
-                  map: map
-              });
-          }
-      </script>
+                var place = new google.maps.LatLng(-38.0526,-57.5614);
+                marker = new google.maps.Marker({
+                    position: place,
+                    title: "sdasdasd",
+                    map: map
+                });
+            }
+        </script>
 
-  </div>
+    </div>
 </div>
