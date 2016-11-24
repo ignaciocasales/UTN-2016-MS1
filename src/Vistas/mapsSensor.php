caa6c39c31f24c27e7395ee3f1e786f1158e379e
navarroddi <davidddi.dn@gmail.com>
2016-11-24 09:04:25 -0300

    Api de google maps funcionando correctamente

--- src/Vistas/mapsSensor.php
+++ src/Vistas/mapsSensor.php
@@ -3,24 +3,25 @@
     <div clas ="container" >
 
 
-        <input type="button" onclick="init()" value="Mostrar mapa">
+        <input type="button" onclick="init(<?= $obtener['longitud'] ?>,<?= $obtener['latitud'] ?>)" value="Mostrar mapa">
         <div id="map" style="width: 1137px; height: 380px;"></div>
 
         <script src="http://maps.google.com/maps/api/js?sensor=false"> </script>
+        <script src="//maps.googleapis.com/maps/api/js?key=AIzaSyDcvicJ0hoFQ8iVcqI1_qyC32bsHPML98I" async="" defer="defer" type="text/javascript"></script>
 
         <script type="text/javascript">
             var map;
             var marker;
 
-            function init() {
+            function init(longitud,latitud) {
                 var mapOptions = {
-                    center: new google.maps.LatLng(-38.0526,-57.5614),
+                    center: new google.maps.LatLng(latitud,longitud),
                     zoom: 17,
                     mapTypeId: google.maps.MapTypeId.ROADMAP
                 }
                 map = new google.maps.Map(document.getElementById("map"),mapOptions);
 
-                var place = new google.maps.LatLng(-38.0526,-57.5614);
+                var place = new google.maps.LatLng(latitud,longitud);
                 marker = new google.maps.Marker({
                     position: place,
                     title: "sdasdasd",
